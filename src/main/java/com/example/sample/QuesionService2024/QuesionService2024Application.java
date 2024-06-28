ffbf4206e0f8c37e5b86010646dd8f441fa9c828
sudhakar0468 <pogula.sudhakarreddy@gmail.com>
2024-06-28 11:49:16 +0530

    git initial commit in quesion service

---
+++ src/main/java/com/example/sample/QuesionService2024/QuesionService2024Application.java
@@ -0,0 +1,13 @@
+package com.example.sample.QuesionService2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuesionService2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuesionService2024Application.class, args);
+	}
+
+}
